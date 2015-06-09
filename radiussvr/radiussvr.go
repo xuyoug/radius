eb13236f4c12bf66d7eb964214d62537ad5e8f2f
xuyoug <xuyoug@yeah.net>
2015-06-10 00:41:48 +0800

    完善注释，些许调整

--- radiussvr/radiussvr.go
+++ radiussvr/radiussvr.go
@@ -12,7 +12,7 @@ import (
 
 //radius服务器的实现封装
 
-//
+//SrcRadius定义收到的radius源报文结构
 type SrcRadius struct {
 	SrcAddr    *net.UDPAddr
 	Secret     string
@@ -22,7 +22,7 @@ type SrcRadius struct {
 	buf        *bytes.Buffer
 }
 
-//
+//ReplyRadius定义响应radius报文结构
 type ReplyRadius struct {
 	DstAddr    *net.UDPAddr
 	Secret     string
@@ -32,7 +32,8 @@ type ReplyRadius struct {
 	buf        *bytes.Buffer
 }
 
-//
+//Reply由源报文生成基础响应报文
+//不带任何属性
 func (sr *SrcRadius) Reply(judge bool) (*ReplyRadius, error) {
 	var err error
 	rr := new(ReplyRadius)
@@ -55,7 +56,7 @@ func (sr *SrcRadius) Reply(judge bool) (*ReplyRadius, error) {
 	return rr, nil //然后交由外部处理
 }
 
-//
+//makebuf字节化响应报文
 func (rr *ReplyRadius) makebuf() {
 	rr.Radius.R_Length = rr.Radius.GetLength()
 	rr.Radius.WriteToBuff(rr.buf)
@@ -67,13 +68,13 @@ func (rr *ReplyRadius) makebuf() {
 	rr.Radius.WriteToBuff(rr.buf)
 }
 
-//
+//Send发送响应报文
 func (dr *ReplyRadius) Send() {
 	dr.makebuf()
 	dr.lisenter.c_send <- dr
 }
 
-//
+//定义radiuslistener的结构
 type RadiusListener struct {
 	conn          *net.UDPConn
 	udpAddr       *net.UDPAddr
@@ -97,13 +98,13 @@ type RadiusListener struct {
 	lsr_sync_f    sync.RWMutex
 }
 
-//
+//定义从网卡获取的原始为格式化的radius信息
 type original_radius struct {
 	udpAddr *net.UDPAddr
 	buf     *bytes.Buffer
 }
 
-//
+//run启动RadiusListener
 func (c *RadiusListener) run(cache int) error {
 	c.C_recive = make(chan *SrcRadius, cache)
 	c.c_or = make(chan *original_radius, cache)
@@ -124,7 +125,8 @@ func (c *RadiusListener) run(cache int) error {
 	return nil
 }
 
-//
+//getSrcOriginalbytes从网卡获取的原始为格式化的radius信息
+//并将其存入缓存chan
 func (c *RadiusListener) getSrcOriginalbytes() {
 	for {
 		var bs [4096]byte
@@ -137,7 +139,7 @@ func (c *RadiusListener) getSrcOriginalbytes() {
 			c.Add_wrong(udpAddr.IP, err)
 			return
 		}
-		if b_num > 4096 || b_num < 20 {
+		if b_num > 4096 || b_num < 20 { //对于长度非法的自己忽略
 			err = radius.ERR_LEN_INVALID
 			c.Add_wrong(udpAddr.IP, err)
 			return
@@ -149,7 +151,8 @@ func (c *RadiusListener) getSrcOriginalbytes() {
 	}
 }
 
-//
+//fmtRadius从缓存chan获取原始radius报文
+//一旦获取数据则启动一个新goroutine对其格式化
 func (c *RadiusListener) fmtRadius() {
 	for {
 		select {
@@ -159,17 +162,17 @@ func (c *RadiusListener) fmtRadius() {
 	}
 }
 
-//
+//decoderadius格式化原始radius报文
 func (c *RadiusListener) decoderadius(or *original_radius) {
 	var ip net.IP
 	var secret string
 	var err error
-	//
+	//添加一个格式化计数
 	c.addfmtgoroutine()
 	src_r := new(SrcRadius)
 
-	ip = or.udpAddr.IP
-	secret = c.secretlist.GetSecret(ip.String())
+	ip = or.udpAddr.IP                           //获取IP
+	secret = c.secretlist.GetSecret(ip.String()) //根据IP获取密钥
 	src_r.Secret = secret
 	//
 
@@ -202,7 +205,8 @@ func (c *RadiusListener) decoderadius(or *original_radius) {
 	}
 }
 
-//
+//作为radiuslistener的发送goroutine
+//将发送队列内的响应报文发送出去
 func (c *RadiusListener) replyRadius() {
 	var err error
 	for {
@@ -217,7 +221,8 @@ func (c *RadiusListener) replyRadius() {
 	}
 }
 
-//
+//RadiusServer生成一个radiuslistener并启动
+//传入参数：启动端口、密钥管理器、超时、队列深度
 func RadiusServer(port int, secret_list *SecretList, timeout time.Duration, cache int) (*RadiusListener, error) {
 	addr := ":" + strconv.Itoa(port)
 	udpAddr, err := net.ResolveUDPAddr("udp4", addr)
