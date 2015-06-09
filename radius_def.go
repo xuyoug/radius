4dcc2abb90693b9776f2bacf6aa11e5afce49b3c
xuyoug <xuyoug@yeah.net>
2015-06-10 00:38:06 +0800

    增加注释

--- radius_def.go
+++ radius_def.go
@@ -9,6 +9,7 @@ import (
 //定义radius的CODE 一个字节
 type R_Code uint8
 
+//定义常见的radiuscode
 const (
 	CodeAccessRequest R_Code = iota + 1
 	CodeAccessAccept
@@ -21,24 +22,28 @@ const (
 	CodeReserved        R_Code = 255
 )
 
-//定义radius的ID 一个字节
+//定义radius的ID
+//一个字节
 type R_Id uint8
 
+//定义radius的Id最大值
 const R_Id_MAX R_Id = 254
 
 //定义radius的Len 两个字节
 type R_Length uint16
 
+//定义radiusLength的最大最小值
 const (
-	radiusLength_MIN R_Length = 20
-	radiusLength_MAX R_Length = 4096
+	R_Length_MIN R_Length = 20
+	R_Length_MAX R_Length = 4096
 )
 
 //定义authenticator 16字节
 type R_Authenticator []byte
 
+//定义Authenticator长度
 const (
-	Radius_Authenticator_LEN = 16
+	R_Authenticator_LEN = 16
 )
 
 //定义radius的基础数据结构
